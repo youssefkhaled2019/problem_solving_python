b18dc94b3c8be24a72ab4f37547f257171c3177a
youssefkhaled2019 <43790986+youssefkhaled2019@users.noreply.github.com>
2023-05-25 22:37:27 +0300

    Add files via upload

---
+++ 4_A.py
@@ -0,0 +1,11 @@
+"""
+contest=https://codeforces.com/contest/4/problem/A
+date= Thursday, May 25, 2023 
+Verdict =
+"""
+n=int(input())
+n_2=(n//2)*2
+if (n==n_2*2 and n!=2):
+    print("YES")
+else:
+    print("NO")    
